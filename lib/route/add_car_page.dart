812bbce86da4a2ed8e9f69801e8efe683b06ece9
Kornél <stefankornel02@gmail.com>
2020-11-14 16:41:55 +0100

    fix: Readded car page

--- lib/route/add_car_page.dart
+++ lib/route/add_car_page.dart
@@ -1,122 +1,21 @@
-import 'dart:io';
+import 'package:duszamobile2020/widgets/car_create_form.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:duszamobile2020/generated/l10n.dart';
 
-class AddCarPage extends StatefulWidget {
-	AddCarPage({Key key}) : super(key: key);
+GlobalKey<ScaffoldState> _profileScaffoldKey = new GlobalKey<ScaffoldState>();
 
-	@override
-	_AddCarPageState createState()=> _AddCarPageState();
-}
-
-class _AddCarPageState extends State<AddCarPage> {
-	TextEditingController textEditingControllerName = TextEditingController();
-	TextEditingController textEditingControllerPrice= TextEditingController();
-	TextEditingController textEditingControllerYear= TextEditingController();
-
-
-	final _picker = ImagePicker();
-	File file;
-
-	final focusNode = FocusNode();
-
-
-	@override
-	Widget build(BuildContext context){
-		return Scaffold(
-				appBar: AppBar(
-					title: Text("Autó hozzáadás"),
-				),
-				body: Padding(
-				  padding: const EdgeInsets.all(16),
-				  child: Column(
-				  	children: [
-				  		Builder(builder: (context){
-				  			if(file!=null){
-				  				return Image.file(file);
-								}
-								return IconButton(icon: Icon(FontAwesomeIcons.image,), iconSize: 80,
-									onPressed: () async {
-										PickedFile pickedImage = await _picker.getImage(source: ImageSource.gallery);
-										if(pickedImage != null){
-											setState(() {
-												file = File(pickedImage.path);
-											});
-										}
-									});
-							}),
-				  		
-				  		Padding(
-				  		  padding: const EdgeInsets.only(top: 10),
-				  		  child: TextField(
-									focusNode: focusNode,
-									autofocus: true,
-				  		  	style: TextStyle(fontSize: 18),
-				  		  	maxLines: 1,
-									onTap: (){
-										focusNode.requestFocus();
-									},
-				  		  	controller: textEditingControllerName,
-				  		  	textInputAction: TextInputAction.next,
-				  		  	decoration:
-				  		  	InputDecoration(labelText: "Név",// helperText: "Oktatási azonositó",
-				  		  		alignLabelWithHint: true,
-				  		  		labelStyle: TextStyle(
-
-				  		  		),
-				  		  		filled: true,
-				  		  		fillColor: Colors.grey.withAlpha(120),
-				  		  	),
-				  		  ),
-				  		),
-				  		Padding(
-				  		  padding: const EdgeInsets.only(top:10),
-				  		  child: TextField(
-				  		  	style: TextStyle(fontSize: 18),
-				  		  	maxLines: 1,
-
-				  		  	controller: textEditingControllerPrice,
-				  		  	textInputAction: TextInputAction.next,
-				  		  	decoration:
-				  		  	InputDecoration(labelText: "Pénz",// helperText: "Oktatási azonositó",
-				  		  		alignLabelWithHint: true,
-				  		  		labelStyle: TextStyle(
-
-				  		  		),
-				  		  		filled: true,
-				  		  		fillColor: Colors.grey.withAlpha(120),
-				  		  	),
-				  		  ),
-				  		),
-				  		Padding(
-				  		  padding: const EdgeInsets.only(top: 10),
-				  		  child: TextField(
-				  		  	style: TextStyle(fontSize: 18),
-				  		  	maxLines: 1,
-				  		  	controller: textEditingControllerYear,
-				  		  	textInputAction: TextInputAction.next,
-				  		  	decoration:
-				  		  	InputDecoration(labelText: "Év",// helperText: "Oktatási azonositó",
-				  		  		alignLabelWithHint: true,
-				  		  		labelStyle: TextStyle(
-
-				  		  		),
-				  		  		filled: true,
-				  		  		fillColor: Colors.grey.withAlpha(120),
-				  		  	),
-				  		  ),
-				  		),
-				  		Spacer(),
-				  		RaisedButton(
-				  			child: Text("Hozzáadás"),
-				  			onPressed: (){
-				  				// notify bloc
-				  			},
-				  		),
-				  	],
-				  ),
-				)
-		);
-	}
+class AddCarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _profileScaffoldKey,
+      appBar: AppBar(
+        title: Text(S.of(context).add_car),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: CarCreateForm(),
+      ),
+    );
+  }
 }
